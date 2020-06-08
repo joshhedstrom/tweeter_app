cda15c39330b302b4d58aa3fdf38236f00d3c22c
Josh Hedstrom <josh.hedstrom@protonmail.com>
2020-06-08 10:02:24 -0600

    adjusted test to reflect new title

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -3,25 +3,25 @@ require 'test_helper'
 class StaticPagesControllerTest < ActionDispatch::IntegrationTest
 
   def setup
-    @base_title = "TweeterApp"
+    @base_title = "Tweeter"
   end
 
   test "should get home" do
     get static_pages_home_url
     assert_response :success
-    assert_select "title", "#{@base_title}"
+    assert_select "title", "#{@base_title} | Home"
   end
 
   test "should get help" do
     get static_pages_help_url
     assert_response :success
-    assert_select "title", "#{@base_title}"
+    assert_select "title", "#{@base_title} | Help"
   end
 
   test "should get about" do
     get static_pages_about_url
     assert_response :success
-    assert_select "title", "#{@base_title}"
+    assert_select "title", "#{@base_title} | About"
   end
 
 end
